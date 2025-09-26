d9383a404e022735e1952781ee24a5560abb20a7
isabel3sanchez <sanchez3isabel@gmail.com>
2025-09-26 13:50:19 -0400

    fifth commit

--- Lab4.py
+++ Lab4.py
@@ -2,19 +2,20 @@
 def fibonacci(integer):
     a = 0
     b = 1
+    result = a + b
     count = 3
+
     if integer == 1:
         return 0
     if integer == 2:
         return 1
+
     while count != integer:
-        a += b
-        b += a
+        a = b
+        b = result
+        result = a + b
         count += 1
-        if integer % 2 == 0:
-            return a
-        else:
-            return b
+    return result
 
 
 #Function 2: Prime Numbers
@@ -30,8 +31,12 @@ def is_prime (integer):
 def print_prime_factors(num):
     a = 1
     while num > 1 and num % a == 0:
-        if num % a == 0:
-            print(a,"*", end='')
+        for i in range(2, num):
+            if num % i == 0:
+                print(i, "*", end= '')
+                num = num // i
+            else:
+                print(f"{num} = {num}")
         break
 
 
